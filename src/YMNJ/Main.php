bd1dd7c2efb9e805c0088a715090385b7470d280
TheDeibo <deiboibbs@gmail.com>
2016-03-14 11:58:56 +0000

    Update Main.php

--- src/YMNJ/Main.php
+++ src/YMNJ/Main.php
@@ -1,28 +1,37 @@
 <?php
 namespace YMNJ;
 	
-use pocketmine\plugin\PluginBase as Plugin;
+use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerKickEvent as PlayerKick;
 
-class Main extends Plugin implements Listener {
+class Main extends PluginBase implements Listener{
     public function onLoad() {
+        $this->getServer()->getLogger()->info("");
+        $this->getServer()->getLogger()->info("");
     	$this->getLogger()->info(TextFormat::LIGHT_PURPLE . "YouMayNowLoad.... I mean Join!.");
+    	$this->getServer()->getLogger()->info("");
+        $this->getServer()->getLogger()->info("");
     }
-    public function onEnable() {
+    public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getLogger()->info("");
         $this->getServer()->getLogger()->info("");
         $this->getLogger()->info(TextFormat::LIGHT_PURPLE . "YouMayNowJoin Is Enabled.");
-                $this->getServer()->getLogger()->info("");
+        $this->getServer()->getLogger()->info("");
+        $this->getServer()->getLogger()->info("");
     }
-    public function onPlayerKick(PlayerKick $event) {
-        if($event->getReason() === "disconnectionScreen.serverFull")
+    public function onPlayerKick(PlayerKick $event){
+        if($event->getReason() === "disconnectionScreen.serverFull"){
             $event->setCancelled(true);
             $this->getLogger()->info(TextFormat::LIGHT_PURPLE . "YouMayMowJoin has been successfully activated.");
+        }
     }
-    public function onDisable() {
-        $this->getServer()->getLogger()->info("YouMayNowJoin has been disabled. Thanks for using YouMayNowJoin.");
-        $this->getLogger()->info(TextFormat::LIGHT_PURPLE . "ServerLoveMCPE is loading.");
+    public function onDisable(){
+    	$this->getServer()->getLogger()->info("");
+        $this->getServer()->getLogger()->info("");
+        $this->getLogger()->info(TextFormat::LIGHT_PURPLE . "YouMayNowJoin has been disabled. Thanks for using YouMayNowJoin.");
+        $this->getServer()->getLogger()->info("");
+        $this->getServer()->getLogger()->info("");
     }
 }
